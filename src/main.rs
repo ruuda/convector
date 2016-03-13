7f748a04c168871b7870e5dd51d09684fc4c8d35
Ruud van Asseldonk <dev@veniogames.com>
2016-03-13 11:16:00 +0100

    Rework patches system
    Stitching patches can now be done in parallel with rendering so it no
    longer blocks rendering of the next frame. This results in a mild
    speedup (~35 fps to ~40 fps), but stitching patches still takes far too
    long: about 20 ms, which is far above my 16.6 ms budget, and texture
    upload has to happen in that time as well.
    
    Also, stitching is currently not complete, so the rendered image looks
    garbled at the moment.

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,11 @@ mod wavefront;
 #[cfg(test)]
 mod bench;
 
-use renderer::Renderer;
+use renderer::{PatchBuffer, Renderer};
 use scene::Scene;
 use stats::GlobalStats;
-use std::slice;
+use std::mem;
 use ui::Window;
-use util::z_order;
 use wavefront::Mesh;
 
 fn build_scene() -> Scene {
@@ -69,107 +68,60 @@ fn main() {
     let mut window = Window::new(width, height, "infomagr interactive raytracer");
     let mut renderer = Renderer::new(build_scene(), width, height);
     let mut stats = GlobalStats::new();
-
-    // Initialize a buffer to black.
-    let screen_len = width as usize * height as usize * 3;
-    let mut frontbuffer = Vec::with_capacity(screen_len);
-    let mut patchbuffer = Vec::with_capacity(screen_len);
-    frontbuffer.resize(screen_len, 0);
-    patchbuffer.resize(screen_len, 0);
-
+    let mut trace_log = trace::TraceLog::with_limit(6 * 1024);
     let mut threadpool = scoped_threadpool::Pool::new(num_cpus::get() as u32);
+    let mut backbuffer = PatchBuffer::new_black(width, height, patch_width);
     let mut should_continue = true;
+
     println!("scene and renderer initialized, entering render loop");
 
-    let mut trace_log = trace::TraceLog::with_limit(6 * 1024);
-
     while should_continue {
+        trace_log.inc_frame_number();
         let stw_frame = trace_log.scoped("render_frame", 0);
 
         renderer.update_scene();
-        {
-            let mut patches = make_slices(&mut patchbuffer[..], (patch_width * patch_width * 3) as usize);
-            let renderer_ref = &renderer;
-            let trace_log_ref = &trace_log;
 
-            threadpool.scoped(|scope| {
-                let mut x = 0;
-                let mut y = 0;
+        let new_backbuffer = PatchBuffer::new_uninit(width, height, patch_width);
+        let frontbuffer = mem::replace(&mut backbuffer, new_backbuffer);
+        let patches = backbuffer.patches();
+        let renderer_ref = &renderer;
+        let trace_log_ref = &trace_log;
 
-                // Queue tasks for the worker threads to render patches.
-                let mut i = 0;
-                for patch in &mut patches {
-                    scope.execute(move || {
-                        let _stw = trace_log_ref.scoped("render_patch", i);
-                        renderer_ref.render_patch(patch, patch_width as u32, x as u32, y as u32);
-                    });
-
-                    i = i + 1;
-                    x = x + patch_width;
-                    if x >= width {
-                        x = 0;
-                        y = y + patch_width;
-                    }
-                }
-
-                {
-                    // In the mean time, upload the previously rendered frame to
-                    // the GPU and display it.
-                    let _stw = trace_log.scoped("display_buffer", 0);
-                    window.display_buffer(frontbuffer, &mut stats);
-                }
-
-                should_continue = window.handle_events(&mut stats, &trace_log);
-
-                // The scope automatically waits for all tasks to complete
-                // before the loop continues.
-            });
-
-            let _stw = trace_log.scoped("stitch_patches", 0);
-
-            // Create a new uninitialized buffer to stitch the patches into.
-            // Because this will write to every pixel, no uninitialized memory
-            // is exposed.
-            let mut backbuffer: Vec<u8> = Vec::with_capacity(screen_len);
-            unsafe { backbuffer.set_len(screen_len); }
-
-            // Stitch together the patches into an image.
+        threadpool.scoped(|scope| {
             let mut x = 0;
             let mut y = 0;
-            for patch in &patches {
-                for i in 0..(patch_width * patch_width) {
-                    let (px, py) = z_order(i as u16);
-                    let bb_idx = ((y + py as u32) * width * 3 + (x + px as u32) * 3) as usize;
-                    backbuffer[bb_idx + 0] = patch[i as usize * 3 + 0];
-                    backbuffer[bb_idx + 1] = patch[i as usize * 3 + 1];
-                    backbuffer[bb_idx + 2] = patch[i as usize * 3 + 2];
-                }
-                x = x + patch_width; // TODO: DRY this up.
+
+            // Queue tasks for the worker threads to render patches.
+            for (i, patch) in (0..).zip(patches) {
+                scope.execute(move || {
+                    let _stw = trace_log_ref.scoped("render_patch", i);
+                    renderer_ref.render_patch(patch, patch_width as u32, x as u32, y as u32);
+                });
+
+                // TODO: DRY.
+                x = x + patch_width;
                 if x >= width {
                     x = 0;
                     y = y + patch_width;
                 }
             }
 
-            frontbuffer = backbuffer;
-        }
+            // In the mean time, stitch together the patches from the previous
+            // frame, upload the frame to the GPU, and display it.
+            let stw_stitch = trace_log.scoped("stitch_patches", 0);
+            let frame = frontbuffer.into_bitmap();
+            stw_stitch.take_duration();
+
+            let stw_display = trace_log.scoped("display_buffer", 0);
+            window.display_buffer(frame, &mut stats);
+            stw_display.take_duration();
+
+            should_continue = window.handle_events(&mut stats, &trace_log);
+
+            // The scope automatically waits for all tasks to complete
+            // before the loop continues.
+        });
 
         stats.frame_us.insert_time_us(stw_frame.take_duration());
-        trace_log.inc_frame_number();
     }
-}
-
-/// Splits the buffer into slices of length `slice_len`.
-///
-/// This is similar to `slice::split_at_mut()`.
-fn make_slices<'a>(backbuffer: &'a mut [u8], slice_len: usize) -> Vec<&'a mut [u8]> {
-    let len = backbuffer.len();
-    let mut ptr = backbuffer.as_mut_ptr();
-    let end = unsafe { ptr.offset(len as isize) };
-    let mut slices = Vec::with_capacity(len / slice_len);
-    while ptr < end {
-        slices.push(unsafe { slice::from_raw_parts_mut(ptr, slice_len) });
-        ptr = unsafe { ptr.offset(slice_len as isize) };
-    }
-    slices
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,5 +1,6 @@
 use scene::Scene;
 use simd::Mf32;
+use std::slice::ChunksMut;
 use time::PreciseTime;
 use util::z_order;
 use vector3::{MVector3, SVector3};
@@ -9,6 +10,102 @@ pub struct Renderer {
     width: u32,
     height: u32,
     epoch: PreciseTime,
+}
+
+/// The buffer that an image is rendered into.
+///
+/// Memory layout is optimized for ray locality.
+pub struct PatchBuffer {
+    buffer: Vec<u8>,
+    patch_width: u32,
+}
+
+impl PatchBuffer {
+    /// Allocates a new patch buffer to render into, memory uninitialized.
+    pub fn new_uninit(width: u32, height: u32, patch_width: u32) -> PatchBuffer {
+        assert_eq!(patch_width & (patch_width - 1), 0); // Patch width must be a power of 2.
+        assert_eq!(patch_width & 3, 0);                 // Patch width must be a multiple of 4.
+        assert_eq!(width & (patch_width - 1), 0);       // With must be a multiple of the patch width.
+        assert_eq!(height & (patch_width - 1), 0);      // Height must be a multiple of the patch width.
+
+        let len = (width as usize) * (height as usize) * 3;
+
+        // Allocate a buffer for the screen. All memory is written to by the
+        // renderer, no uninitialized memory is exposed.
+        let mut buffer = Vec::with_capacity(len);
+        unsafe { buffer.set_len(len); }
+
+        PatchBuffer {
+            buffer: buffer,
+            patch_width: patch_width
+        }
+    }
+
+    /// Allocates a new patch buffer to render into, initialized to black.
+    pub fn new_black(width: u32, height: u32, patch_width: u32) -> PatchBuffer {
+        let mut patchbuffer = PatchBuffer::new_uninit(width, height, patch_width);
+        let len = patchbuffer.buffer.len();
+        patchbuffer.buffer.clear();
+        patchbuffer.buffer.resize(len, 0);
+        patchbuffer
+    }
+
+    /// Returns slices which are the patches in this buffer.
+    ///
+    /// The order of the patches is from left to right and from bottom to top.
+    /// So the bottom-left pixel of the first patch is at (0, 0) and the
+    /// top-right pixel of the last patch is at (width, height).
+    ///
+    /// For every patch, the memory layout is as follows: the patch is divided
+    /// into blocks of 4x2 pixels. The pixels in these blocks are stored RGB
+    /// from left to right and from bottom to top. These blocks then fill the
+    /// patch in a Z-order starting at the bottom-left block.
+    pub fn patches(&mut self) -> ChunksMut<u8> {
+        let patch_len = (self.patch_width * self.patch_width * 3) as usize;
+        self.buffer.chunks_mut(patch_len)
+    }
+
+    fn swap_mem(xs: &mut [u8], ys: &mut [u8]) {
+        use std::mem;
+        for (x, y) in xs.iter_mut().zip(ys.iter_mut()) {
+            mem::swap(x, y);
+        }
+    }
+
+    /// Converts the patches from z-order memory order to bitmap order.
+    fn untangle_patches(&mut self) {
+        let width = self.patch_width;
+        let patch_len = (width * width * 3) as usize;
+
+        // Create a temporary buffer to untangle the patch into.
+        // TODO: It must be possible to do this in-place using a recursive
+        // algorithm.
+        let mut scratch = Vec::with_capacity(patch_len);
+        unsafe { scratch.set_len(patch_len); }
+
+        for patch in self.patches() {
+            for i in 0..(width * width) {
+                let (px, py) = z_order(i as u16);
+                let idx = ((py as u32) * width * 3 + (px as u32) * 3) as usize;
+                scratch[idx + 0] = patch[i as usize * 3 + 0];
+                scratch[idx + 1] = patch[i as usize * 3 + 1];
+                scratch[idx + 2] = patch[i as usize * 3 + 2];
+            }
+
+            PatchBuffer::swap_mem(patch, &mut scratch[..]);
+        }
+    }
+
+    fn stitch_patches(&mut self) {
+        // TODO.
+    }
+
+    /// Stitches the internal representation into an RGB image.
+    pub fn into_bitmap(mut self) -> Vec<u8> {
+        self.untangle_patches();
+        self.stitch_patches();
+        self.buffer
+    }
 }
 
 impl Renderer {
@@ -58,8 +155,6 @@ impl Renderer {
     /// patch. The patch width must be a power of 16.
     pub fn render_patch(&self, patch: &mut [u8], patch_width: u32, x: u32, y: u32) {
         assert_eq!(patch.len(), (3 * patch_width * patch_width) as usize);
-        assert_eq!(patch_width & (patch_width - 1), 0); // Patch width must be a power of 2.
-        assert_eq!(patch_width & 3, 0); // Patch width must be a multiple of 4.
 
         let n = patch_width / 4;
         for i in 0..(n * n) {
