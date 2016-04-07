6d637ec7fd5a2059c1a6a142e0e48b2e236cfb42
Ruud van Asseldonk <dev@veniogames.com>
2016-04-07 22:03:46 +0200

    Implement generating random hemisphere vectors

--- src/random.rs
+++ src/random.rs
@@ -6,7 +6,9 @@
 //! RNGs.
 
 use simd::{Mf32, Mi32, Mu64};
+use std::f32::consts;
 use std::i32;
+use vector3::MVector3;
 
 #[cfg(test)]
 use test;
@@ -89,6 +91,72 @@ impl Rng {
 
         mi32.into_mf32() * range
     }
+
+    /// Returns 8 random numbers distributed uniformly over the half-open
+    /// interval [-pi, pi).
+    pub fn sample_angle(&mut self) -> Mf32 {
+        use std::mem::transmute;
+
+        let mi32: Mi32 = unsafe { transmute(self.next()) };
+        let range = Mf32::broadcast(consts::PI / i32::MIN as f32);
+
+        mi32.into_mf32() * range
+    }
+
+    /// Returns a random unit vector in the hemisphere around the positive
+    /// z-axis, drawn from a cosine-weighted distribution.
+    pub fn sample_hemisphere_vector(&mut self) -> MVector3 {
+        let phi = self.sample_angle();
+        let r_sqr = self.sample_unit();
+
+        // Instead of the full square root, we could also do a fast inverse
+        // square root approximation and a reciprocal approximation. It is less
+        // precise, but according to the Intel intrinsics guide, that would take
+        // 14 cycles instead of 21. However, we need to compute the polynomials
+        // for sin and cos anyway and that takes time, so it is not a problem to
+        // take the slow but precise square root: by the time we need it, plenty
+        // of cycles will have passed. Pipelining to the rescue here.
+        let r = r_sqr.sqrt();
+        let x = phi.sin() * r;
+        let y = phi.cos() * r; // TODO: cos is a bottleneck, do I need the precision?
+        let z = (Mf32::one() - r_sqr).sqrt();
+        MVector3::new(x, y, z)
+    }
+
+    /// Returns a random unit vector in the hemisphere around the positive
+    /// z-axis, drawn from a cosine-weighted distribution.
+    ///
+    /// This method uses a different sampling method than
+    /// `sample_hemisphere_vector`. Benchmarks show that it is not faster, and
+    /// with a small probability this function returns a wrong result too, so it
+    /// should not be used at all. It is kept here for comparison purposes.
+    fn sample_hemisphere_vector_reject(&mut self) -> MVector3 {
+        // This function uses rejection sampling without branching: sample two
+        // points in a square, and if the second one is not inside a circle,
+        // take the first one instead. The probability that both points do not
+        // lie in a circle is (1 - pi/4)^2, about 4.6%. To reduce that
+        // probability further you can take more samples.
+        let x0 = self.sample_biunit();
+        let y0 = self.sample_biunit();
+        let r0 = x0.mul_add(x0, y0 * y0);
+
+        let x1 = self.sample_biunit();
+        let y1 = self.sample_biunit();
+        let r1 = x1.mul_add(x1, y1 * y1);
+
+        // If r1 > 1, then the point lies outside of a unit disk, so the sign
+        // bit of this value will be positive, indicating that we should pick
+        // point 0 instead of point 1.
+        let pick_01 = Mf32::one() - r1;
+
+        let x = x0.pick(x1, pick_01);
+        let y = y0.pick(y1, pick_01);
+        let r = r0.pick(r1, pick_01);
+
+        let z = (Mf32::one() - r).sqrt();
+
+        MVector3::new(x, y, z)
+    }
 }
 
 #[test]
@@ -97,8 +165,8 @@ fn sample_unit_is_in_interval() {
 
     for _ in 0..4096 {
         let x = rng.sample_unit();
-        assert!(x.all_sign_bits_positive(), "{:?} should be >= 0");
-        assert!((Mf32::one() - x).all_sign_bits_positive(), "{:?} should be <= 1");
+        assert!(x.all_sign_bits_positive(), "{:?} should be >= 0", x);
+        assert!((Mf32::one() - x).all_sign_bits_positive(), "{:?} should be <= 1", x);
     }
 }
 
@@ -108,8 +176,31 @@ fn sample_biunit_is_in_interval() {
 
     for _ in 0..4096 {
         let x = rng.sample_biunit();
-        assert!((Mf32::one() + x).all_sign_bits_positive(), "{:?} should be >= -1");
-        assert!((Mf32::one() - x).all_sign_bits_positive(), "{:?} should be <= 1");
+        assert!((Mf32::one() + x).all_sign_bits_positive(), "{:?} should be >= -1", x);
+        assert!((Mf32::one() - x).all_sign_bits_positive(), "{:?} should be <= 1", x);
+    }
+}
+
+#[test]
+fn sample_angle_is_in_interval() {
+    let mut rng = Rng::with_seed(2, 5, 7);
+
+    for _ in 0..4096 {
+        let x = rng.sample_angle();
+        assert!((Mf32::broadcast(consts::PI) + x).all_sign_bits_positive(), "{:?} should be >= -pi", x);
+        assert!((Mf32::broadcast(consts::PI) - x).all_sign_bits_positive(), "{:?} should be <= pi", x);
+    }
+}
+
+#[test]
+fn sample_hemisphere_vector_has_unit_norm() {
+    let mut rng = Rng::with_seed(2, 5, 7);
+
+    for _ in 0..4096 {
+        let v = rng.sample_hemisphere_vector();
+        let r = v.norm_squared().sqrt();
+        assert!((r - Mf32::broadcast(0.991)).all_sign_bits_positive(), "{:?} should be ~1", r);
+        assert!((Mf32::broadcast(1.009) - r).all_sign_bits_positive(), "{:?} should be ~1", r);
     }
 }
 
@@ -126,6 +217,30 @@ fn bench_sample_unit_1000(b: &mut test::Bencher) {
         for _ in 0..100 {
             unroll_10! {{
                 test::black_box(rng.sample_unit());
+            }};
+        }
+    });
+}
+
+#[bench]
+fn bench_sample_hemisphere_vector_1000(b: &mut test::Bencher) {
+    let mut rng = Rng::with_seed(2, 5, 7);
+    b.iter(|| {
+        for _ in 0..100 {
+            unroll_10! {{
+                test::black_box(rng.sample_hemisphere_vector());
+            }};
+        }
+    });
+}
+
+#[bench]
+fn bench_sample_hemisphere_vector_reject_1000(b: &mut test::Bencher) {
+    let mut rng = Rng::with_seed(2, 5, 7);
+    b.iter(|| {
+        for _ in 0..100 {
+            unroll_10! {{
+                test::black_box(rng.sample_hemisphere_vector_reject());
             }};
         }
     });
