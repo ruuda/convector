ef008003a692b702e2e233f5f771791cd8c5eead
Ruud van Asseldonk <dev@veniogames.com>
2016-04-18 22:07:40 +0200

    Apply median in CIE XYZ color space

--- src/gpu/median.glsl
+++ src/gpu/median.glsl
@@ -25,6 +25,22 @@ void sort(inout vec4 a0, inout vec4 a1, inout vec4 a2, inout vec4 a3, inout vec4
     sort2(a3, a4);
 }
 
+vec4 rgb_to_xyz(vec4 c) {
+    mat3 conv = mat3(0.49f, 0.17697f, 0.0f,
+                     0.31f, 0.81240f, 0.01f,
+                     0.20f, 0.01063f, 0.99f) * (1.0f / 0.17697f);
+    c.xyz = conv * c.rgb;
+    return c;
+}
+
+vec4 xyz_to_rgb(vec4 c) {
+    mat3 conv = mat3(0.41847f, -0.091169f, 0.00092090f,
+                     -0.15866f, 0.25243f, -0.0025498f,
+                     -0.082835, 0.015708, 0.17860);
+    c.rgb = conv * c.xyz;
+    return c;
+}
+
 void main() {
     // Sample 5 pixels in a "+" shape.
     vec4 c0 = texture(frame, v_tex_coords);
@@ -33,9 +49,20 @@ void main() {
     vec4 c3 = texture(frame, v_tex_coords - vec2(pixel_size.x, 0.0f));
     vec4 c4 = texture(frame, v_tex_coords - vec2(0.0f, pixel_size.y));
 
-    // Take the median of those pixels.
-    sort(c0, c1, c2, c3, c4);
-    vec4 median = c2;
+    // Convert all the pixels from CIE 1931 to the CIE XYZ color space before
+    // taking the median. This ensures that lightness is better preserved.
+    c0 = rgb_to_xyz(c0);
+    c1 = rgb_to_xyz(c1);
+    c2 = rgb_to_xyz(c2);
+    c3 = rgb_to_xyz(c3);
+    c4 = rgb_to_xyz(c4);
 
-    color = median;
+    // Take the sort-of-median of those pixels. The true median is c2, but do
+    // weigh in a bit of the other pixels as well for a more balanced result.
+    sort(c0, c1, c2, c3, c4);
+    vec4 median = c2 * 0.667f + c1 * 0.1666f + c3 * 0.1666f;
+
+    // Convert back from CIE XYZ to CIE 1931 (which is a linear RGB color
+    // space).
+    color = xyz_to_rgb(median);
 }
