6a970e70014660407bbaea230cedd7bdacd3648e
Ruud van Asseldonk <dev@veniogames.com>
2016-04-18 21:32:57 +0200

    Remove temporal median from frame blending
    It works reasonably well when there is little noise, but the mean works
    better when there is mostly black with a few outliers.

--- src/gpu/blend.glsl
+++ src/gpu/blend.glsl
@@ -12,43 +12,6 @@ uniform sampler2D frame5;
 uniform sampler2D frame6;
 uniform sampler2D frame7;
 
-void sort2(inout vec4 a0, inout vec4 a1) {
-    vec4 b0 = min(a0, a1);
-    vec4 b1 = max(a0, a1);
-    a0 = b0;
-    a1 = b1;
-}
-
-void sort(inout vec4 a0, inout vec4 a1,
-          inout vec4 a2, inout vec4 a3,
-          inout vec4 a4, inout vec4 a5,
-          inout vec4 a6, inout vec4 a7) {
-    sort2(a0, a7);
-    sort2(a1, a6);
-    sort2(a2, a5);
-    sort2(a3, a4);
-
-    sort2(a0, a3);
-    sort2(a4, a7);
-    sort2(a1, a2);
-    sort2(a5, a6);
-
-    sort2(a0, a1);
-    sort2(a2, a3);
-    sort2(a4, a5);
-    sort2(a6, a7);
-
-    sort2(a2, a4);
-    sort2(a3, a5);
-
-    sort2(a1, a2);
-    sort2(a3, a4);
-    sort2(a5, a6);
-
-    sort2(a2, a3);
-    sort2(a3, a5);
-}
-
 void main() {
     vec4 c0 = texture(frame0, v_tex_coords);
     vec4 c1 = texture(frame1, v_tex_coords);
@@ -58,11 +21,7 @@ void main() {
     vec4 c5 = texture(frame5, v_tex_coords);
     vec4 c6 = texture(frame6, v_tex_coords);
     vec4 c7 = texture(frame7, v_tex_coords);
-    sort(c0, c1, c2, c3, c4, c5, c6, c7);
-    // color = (c0 + c1 + c2 + c3 + c4 + c5 + c6 + c7) * 0.125f;
-    if (v_tex_coords.x > 0.8) {
-        color = (c4 + c5) * 0.5;
-    } else {
-        color = (c0 + c1 + c2 + c3 + c4 + c5 + c6 + c7) * 0.125f;
-    }
+
+    // Take the mean of the eight frames.
+    color = (c0 + c1 + c2 + c3 + c4 + c5 + c6 + c7) * 0.125f;
 }
